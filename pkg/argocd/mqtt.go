f1d226168fd2e938138254d3adda3f42edbea22d
Taylor Jones <taylor@tjon.es>
2025-04-06 17:53:22 +0000

    Add imports

--- pkg/argocd/mqtt.go
+++ pkg/argocd/mqtt.go
@@ -2,7 +2,11 @@ package argocd
 
 import (
 	"github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
-	"github.com/eclipse-paho/paho/autopaho"
+	"github.com/eclipse/paho.golang/autopaho"
+	"github.com/eclipse/paho.golang/paho"
+	"github.com/eclipse/paho.golang/paho/log"
+	"github.com/eclipse/paho.golang/paho/session/state"
+	storefile "github.com/eclipse/paho.golang/paho/store/file"
 )
 
 func notifyChangesMqtt(app *v1alpha1.Application, wbc *WriteBackConfig, changeList []ChangeEntry) error {
